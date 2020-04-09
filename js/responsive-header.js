770c9603306849d31027ffef2845e8037ff0355e
Somnath <sahasomnath93@gmail.com>
2020-04-10 02:30:19 +0530

    nav bar

--- js/responsive-header.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("mynavbarH");
-  if (x.className === "navbarH") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbarH";
-  }
-}
