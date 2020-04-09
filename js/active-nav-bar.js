9f9c9cdd386f060dcfb9b6edcef7fda3a72cd0d8
Somnath <sahasomnath93@gmail.com>
2020-04-09 23:01:45 +0530

    active nav

--- js/active-nav-bar.js
+++ js/active-nav-bar.js
@@ -1,9 +1,5 @@
-var header = document.getElementById("mynavbarH");
-var btns = header.getElementsByClassName("navbarH");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
+$("a").each(function() {
+    if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
+        $(this).addClass('active');
+    }
+});
