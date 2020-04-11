1fa12d2bf5a350240bb4a74ab8498c55feb652fc
Somnath <sahasomnath93@gmail.com>
2020-04-11 23:31:57 +0530

    11/4 fix founder section

--- js/custom.js
+++ js/custom.js
@@ -52,12 +52,14 @@ $(document).ready(function() {
   $(document).ready(function() {
     $("#owl-speakers").owlCarousel({
       autoPlay: 6000,
-      items : 4,
+      items : 3,
       itemsDesktop : [1199,2],
       itemsDesktopSmall : [979,1],
       itemsTablet: [768,1],
       itemsTabletSmall: [985,2],
       itemsMobile : [479,1],
+		margin:0,
+    
     });
   });
 
