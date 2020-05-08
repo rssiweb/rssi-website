73393df95f41eea7ff623bc74bb489c339dbd9db
Somnath <sahasomnath93@gmail.com>
2020-05-09 02:28:57 +0530

    home

--- js/carousel.js
+++ js/carousel.js
@@ -1,23 +1,28 @@
 //Logo slider//
-$(document).ready(function(){
-    $('.customer-logos').slick({
-      slidesToShow: 7,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1000,
-      arrows: false,
-      dots: false,
-        pauseOnHover: true,
-        responsive: [{
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3
-        }
-      }, {
-        breakpoint: 520,
-        settings: {
-          slidesToShow: 2
-        }
-      }]
-    });
+$(document).ready(function () {
+  $('.customer-logos').slick({
+
+
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    dots: false,
+    pauseOnHover: true,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 2
+      }
+    }]
   });
+});
