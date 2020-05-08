988b274e8d8fbc2417b7dc2da16d810c476ea0d2
Somnath <sahasomnath93@gmail.com>
2020-05-08 13:07:12 +0530

    home page client logo

--- js/carousel.js
+++ js/carousel.js
@@ -1,22 +1,23 @@
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  navText: [
-    "<i class='fa fa-caret-left'></i>",
-    "<i class='fa fa-caret-right'></i>"
-  ],
-  autoplay: true,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 3
-    },
-    1000: {
-      items: 5
-    }
-  }
-})
+//Logo slider//
+$(document).ready(function(){
+    $('.customer-logos').slick({
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
+      dots: false,
+        pauseOnHover: true,
+        responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 2
+        }
+      }]
+    });
+  });
