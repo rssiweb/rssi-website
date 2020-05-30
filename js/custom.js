4760f190a12feabd99e6c22c190dcff481049244
Somnath Saha <sahasomnath93@gmail.com>
2020-05-31 00:37:32 +0530

    disaableBackground

--- js/custom.js
+++ js/custom.js
@@ -1,35 +1,35 @@
 
- /* jQuery Pre loader
-  -----------------------------------------------*/
-$(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+/* jQuery Pre loader
+ -----------------------------------------------*/
+$(window).load(function () {
+  $('.preloader').fadeOut(1000); // set duration in brackets    
 });
 
 
 /* Mobile Navigation
     -----------------------------------------------*/
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+$(window).scroll(function () {
+  if ($(".navbar").offset().top > 50) {
+    $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else {
+    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  }
 });
 
 
 /* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
-$(document).ready(function() {
+$(document).ready(function () {
 
   /* Hide mobile menu after clicking on a link
     -----------------------------------------------*/
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+  $('.navbar-collapse a').click(function () {
+    $(".navbar-collapse").collapse('hide');
+  });
 
 
- /* Parallax section
-    -----------------------------------------------*/
+  /* Parallax section
+     -----------------------------------------------*/
   function initParallax() {
     $('#intro').parallax("100%", 0.1);
     $('#overview').parallax("100%", 0.3);
@@ -47,41 +47,41 @@ $(document).ready(function() {
   initParallax();
 
 
-    /* Owl Carousel
-  -----------------------------------------------*/
+  /* Owl Carousel
+-----------------------------------------------*/
 
- $(document).ready(function() {
+  $(document).ready(function () {
     $("#owl-speakers").owlCarousel({
       autoPlay: 6000,
-      items : 7, //10 items above 1000px browser width
-      itemsDesktop : [1000,5], //5 items between 1000px and 901px
-      itemsDesktopSmall : [900,3], // betweem 900px and 601px
-      itemsTablet: [600,2], //2 items between 600 and 0
-      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+      items: 7, //10 items above 1000px browser width
+      itemsDesktop: [1000, 5], //5 items between 1000px and 901px
+      itemsDesktopSmall: [900, 3], // betweem 900px and 601px
+      itemsTablet: [600, 2], //2 items between 600 and 0
+      itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
     });
   });
 
   /* Back top
   -----------------------------------------------*/
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
-        $('.go-top').fadeIn(200);
-        } else {
-          $('.go-top').fadeOut(200);
-        }
-        });   
-        // Animate the scroll to top
-      $('.go-top').click(function(event) {
-        event.preventDefault();
-      $('html, body').animate({scrollTop: 0}, 300);
-      })
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $('.go-top').fadeIn(200);
+    } else {
+      $('.go-top').fadeOut(200);
+    }
+  });
+  // Animate the scroll to top
+  $('.go-top').click(function (event) {
+    event.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 300);
+  })
 
 
   /* wow
   -------------------------------*/
   new WOW({ mobile: false }).init();
 
-  });
+});
 
 //RESPONSIVE HEADER//
 function myFunction1() {
@@ -102,19 +102,17 @@ function myFunction() {
   }
 }
 
-//popup image//
+//popup image
 
-$(document).ready(function(){
-  
-  $("#thover").click(function(){
-		$(this).fadeOut();
+//$("#thover").click(function(){
+//$(this).fadeOut();
+//$("#tpopup").fadeOut();
+//});
+
+$(document).ready(function () {
+  $("#tclose").click(function () {
+    $("#thover").fadeOut();
     $("#tpopup").fadeOut();
-	});
-  
-  
-  $("#tclose").click(function(){
-		$("#thover").fadeOut();
-    $("#tpopup").fadeOut();
-	});
-  
+  });
+
 });
