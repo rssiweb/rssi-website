4c449559a96c535c40888d0fb3f5f3137a26f4bf
Somnath Saha <sahasomnath93@gmail.com>
2020-06-06 21:44:54 +0530

    login

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import hashlib
 import click
 import os
 
+autorefresh = os.getenv('AUTO_REFRESH') == 'True'
 app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app)
+app.wsgi_app = WhiteNoise(app.wsgi_app, autorefresh=autorefresh)
 my_static_folders = (
     ('templates/css', 'css'),
     ('templates/js', 'js'),
@@ -19,20 +20,24 @@ for folder, prefix in my_static_folders:
 def public_pages(name='index'):
     return render_template(name + '.html' if '.' not in name else name)
 
-@app.route('/secure/<string:name>')
+@app.route('/secure/<string:name>', methods=['GET', 'POST'])
 def secure_pages(name):
-    code = request.form.get('code')
-    if code:
-        salt = os.getenv('SALT')
-        content = (salt + code).encode()
-        hashed_code = hashlib.sha512(content).hexdigest()
-        if hashed_code == os.getenv('CODE'):
-            if '.' not in name:
-                name = 'secure/' + name + '.html' 
+    user = request.form.get('user')
+    if user != 'admin':
+        name = 'secure/unauthorized.html'
+    else:
+        code = request.form.get('code')
+        if code:
+            salt = os.getenv('SALT')
+            content = (salt + code).encode()
+            hashed_code = hashlib.sha512(content).hexdigest()
+            if hashed_code == os.getenv('CODE'):
+                if '.' not in name:
+                    name = 'secure/' + name + '.html' 
+            else:
+                name = 'secure/unauthorized.html'
         else:
             name = 'secure/unauthorized.html'
-    else:
-        name = 'secure/unauthorized.html'
     return render_template(name)
 
 
