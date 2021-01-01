aa1192416a2d28bdf99cae03544e85f3dedda744
Somnath Saha <sahasomnath93@gmail.com>
2021-01-01 17:12:40 +0530

    ok

--- app.py
+++ app.py
@@ -4,47 +4,58 @@ import hashlib
 import click
 import os
 
-autorefresh = os.getenv('AUTO_REFRESH') == 'True'
+autorefresh = os.getenv("AUTO_REFRESH") == "True"
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, autorefresh=autorefresh)
 my_static_folders = (
-    ('templates/css', 'css'),
-    ('templates/js', 'js'),
-    ('templates/images', 'images')
+    ("templates/css", "css"),
+    ("templates/js", "js"),
+    ("templates/images", "images"),
 )
 for folder, prefix in my_static_folders:
     app.wsgi_app.add_files(folder, prefix)
 
-@app.route('/<string:name>')
-@app.route('/')
-def public_pages(name='index'):
-    return render_template(name + '.html' if '.' not in name else name)
 
-@app.route('/secure/<string:name>', methods=['GET', 'POST'])
-def secure_pages(name):
-    user = request.form.get('user')
-    if user != 'indigo':
-        name = 'secure/unauthorized.html'
+def is_authorized(folder):
+    """
+    given a folder it checks the username and password from 2 environment variables
+    - <FOLDER>_USER to contain the username for this folder
+    - <USER>_CODE to contain the hashed code for the user
+    """
+    user = request.form.get("user")
+    user_for_folder = os.getenv(f"{folder.upper()}_USER", None)
+    if not user_for_folder or not user or user != user_for_folder:
+        return False
     else:
-        code = request.form.get('code')
-        if code:
-            salt = os.getenv('SALT')
-            content = (salt + code).encode()
-            hashed_code = hashlib.sha512(content).hexdigest()
-            if hashed_code == os.getenv('CODE'):
-                if '.' not in name:
-                    name = 'secure/' + name + '.html' 
-            else:
-                name = 'secure/unauthorized.html'
-        else:
-            name = 'secure/unauthorized.html'
+        code = request.form.get("code")
+        if not code:
+            return False
+        salt = os.getenv("SALT")
+        content = (salt + code).encode()
+        hashed_code = hashlib.sha512(content).hexdigest()
+        return hashed_code == os.getenv(f"{user.upper()}_CODE")
+
+
+@app.route("/<string:name>")
+@app.route("/")
+def public_pages(name="index"):
+    return render_template(name + ".html" if "." not in name else name)
+
+
+@app.route("/<string:folder>/<string:name>", methods=["GET", "POST"])
+def secure_pages(folder, name):
+    if not is_authorized(folder):
+        name = f"{folder}/unauthorized.html"
+    else:
+        if "." not in name:
+            name = f"{folder}/{name}.html"
     return render_template(name)
 
 
 @app.cli.command("generate-hash-for")
 @click.argument("code")
 def set_code(code):
-    salt = os.getenv('SALT')
+    salt = os.getenv("SALT")
     content = (salt + code).encode()
     hashed_code = hashlib.sha512(content).hexdigest()
     print(hashed_code)
