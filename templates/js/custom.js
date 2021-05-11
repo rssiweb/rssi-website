03ea6efd31d1a095389c11933880610e65060bd8
Somnath Saha <sahasomnath93@gmail.com>
2021-05-11 14:46:51 +0530

    ok

--- templates/js/custom.js
+++ templates/js/custom.js
@@ -49,18 +49,6 @@ $(document).ready(function() {
 
     /* Back top
     -----------------------------------------------*/
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
-            $('.go-top').fadeIn(200);
-        } else {
-            $('.go-top').fadeOut(200);
-        }
-    });
-    // Animate the scroll to top
-    $('.go-top').click(function(event) {
-        event.preventDefault();
-        $('html, body').animate({ scrollTop: 0 }, 500);
-    })
 
 
     /* wow
