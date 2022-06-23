7e8065bd5b222af769f36ad690e73a14cbeaf2fd
Somnath Saha <sahasomnath93@gmail.com>
2022-06-23 10:00:25 +0530

    ok

--- rssiweb/rssiweb/templates/js/active-nav-bar.js
+++ rssiweb/rssiweb/templates/js/active-nav-bar.js
@@ -1,3 +1,39 @@
+$("#dotted").each(function() {
+    if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
+        $(this).addClass('dotted');
+    }
+});
+
+$("#dotted1").each(function() {
+    if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
+        $(this).addClass('dotted');
+    }
+});
+
+$("#dotted2").each(function() {
+    if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
+        $(this).addClass('dotted');
+    }
+});
+
+$("#dotted3").each(function() {
+    if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
+        $(this).addClass('dotted');
+    }
+});
+
+$("#dotted4").each(function() {
+    if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
+        $(this).addClass('dotted');
+    }
+});
+
+$("#dotted5").each(function() {
+    if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
+        $(this).addClass('dotted');
+    }
+});
+
 $("a").each(function() {
     if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
         $(this).addClass('active');
